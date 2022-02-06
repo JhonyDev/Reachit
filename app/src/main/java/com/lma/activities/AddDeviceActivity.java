6d42c41a22976144beea87649f518cc23b1caeeb
jhonydev <star.jk901@gmail.com>
2022-02-06 22:43:48 +0500

    developing - 60% - changes -> Add device activity added

--- app/src/main/java/com/lma/activities/AddDeviceActivity.java
+++ app/src/main/java/com/lma/activities/AddDeviceActivity.java
@@ -3,6 +3,7 @@ package com.lma.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.lma.R;
 
@@ -12,5 +13,9 @@ public class AddDeviceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_device);
+    }
+
+    public void back(View view) {
+        finish();
     }
 }
